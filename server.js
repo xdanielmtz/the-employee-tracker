4719d5501ee4efab2e7735e0a621879df70a6f45
Dani JM <xdanielmtz@gmail.com>
2020-11-22 22:12:53 -0500

    added the prompt inside a function. added a exit function to end the connection

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const { inherits } = require("util");
+
+
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -15,58 +18,66 @@ var connection = mysql.createConnection({
   database: "ice_creamDB",
 });
 
+
+
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "choice",
-      choices: [
-        "View all Employees",
-        "View all Employees by Department",
-        "View all Employees by Manager",
-        "Add Employee",
-        "Remove Employee",
-        "Update Employee Role",
-        "Update Employee Manager"
-      ],
-    },
-  ]).then((initialRes) => {
-        switch(initialRes.choice){
-            case "Remove Employee":
-                removeEmployee ();
-                break;
-            case "View all Employees":
-                viewAll();
-                break;
-            case "View all Employees by Department":
-                viewByDep();
-                break;
-            case "View all Employees by Manager":
-                viewByManager();
-                break;
-            case "Add Employee":
-                addEmployee();
-                break;
-            case "Update Employee Role":
-                updateRole();
-                break;
-            case "Update Employee Manager":
-                updateManager();
-                break;
-
-        }
-  })
-
-
-
-  connection.end();
+  init();
 });
 
 
-// .then((initialRes) => {
-//     if (initialRes.choice === "Add Employee"){
-//         console.log("correct");
-//     }
-// })
+
+function init (){
+    inquirer.prompt([
+        {
+          type: "list",
+          name: "choice",
+          choices: [
+            "View all Employees",
+            "View all Employees by Department",
+            "View all Employees by Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "EXIT"
+          ],
+        },
+      ]).then((initialRes) => {
+            switch(initialRes.choice){
+                case "Remove Employee":
+                    removeEmployee ();
+                    break;
+                case "View all Employees":
+                    viewAll();
+                    break;
+                case "View all Employees by Department":
+                    viewByDep();
+                    break;
+                case "View all Employees by Manager":
+                    viewByManager();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateRole();
+                    break;
+                case "Update Employee Manager":
+                    updateManager();
+                    break;
+                case "EXIT":
+                    exitApp();
+                    break;
+            }
+      })    
+}
+
+
+
+
+
+function exitApp (){
+    connection.end();
+}
